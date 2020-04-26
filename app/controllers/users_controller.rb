46bf8a46fae4e0954bfefc90442e1b2e25f468b9
celtic93 <egorka80@list.ru>
2020-04-26 13:11:36 +0300

    Added users/edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,21 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: %i(show current_user_home)
+  before_action :find_user
+  #before_action :check_author
+
   def show
-    @user = User.find(params[:id])
+  end
+
+  def edit
   end
 
   def current_user_home
     redirect_to current_user || new_user_registration_path
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
   end
 end
