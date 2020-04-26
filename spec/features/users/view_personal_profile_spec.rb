5a0235041e2df1de710253364d5fc42626534235
celtic93 <egorka80@list.ru>
2020-04-26 23:44:17 +0300

    Added user show spec

--- spec/features/users/view_personal_profile_spec.rb
+++ spec/features/users/view_personal_profile_spec.rb
@@ -6,23 +6,7 @@ feature 'User can see personal profile' do
   scenario 'Authenticated user tryes to see personal profile' do
     sign_in(users[0])
     visit user_path(users[1])
-
-    expect(page).to have_content users[1].firstname
-    expect(page).to have_content users[1].lastname
-    expect(page).to have_content users[1].username
-    expect(page).to have_content users[1].info
-
-    expect(page).to_not have_content users[0].firstname
-    expect(page).to_not have_content users[0].lastname
-    expect(page).to_not have_content users[0].username
-    expect(page).to_not have_content users[0].info
-
     click_on 'My profile'
-
-    expect(page).to_not have_content users[1].firstname
-    expect(page).to_not have_content users[1].lastname
-    expect(page).to_not have_content users[1].username
-    expect(page).to_not have_content users[1].info
 
     expect(page).to have_content users[0].firstname
     expect(page).to have_content users[0].lastname
@@ -30,7 +14,7 @@ feature 'User can see personal profile' do
     expect(page).to have_content users[0].info
   end
 
-  scenario 'Authenticated user tryes to see personal profile' do
+  scenario 'Unauthenticated user tryes to see personal profile' do
     visit user_path(users[0])
     expect(page).to_not have_link 'My profile'
   end
