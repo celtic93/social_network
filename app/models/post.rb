08b73056aeb6a74d25a83e3c89bc275d8ec8c19c
celtic93 <egorka80@list.ru>
2020-04-28 13:39:05 +0300

    Added post ordering

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :body, presence: true
+
+  default_scope { order(created_at: :desc) }
 end
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :post do
-    
+    body { 'Post body' }
+    user
   end
 end
