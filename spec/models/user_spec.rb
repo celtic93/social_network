43c3c4e21f5f1cb15dad583665cd6a26b63b5caf
celtic93 <egorka80@list.ru>
2020-04-28 11:32:02 +0300

    Added post model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
+    it { should have_many :posts }
     it { should validate_presence_of :firstname }
     it { should validate_presence_of :lastname }
     it { should validate_presence_of :username }
