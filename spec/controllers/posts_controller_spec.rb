0071fc4cb5efea72d43a77b668be5ebb69fdd758
celtic93 <egorka80@list.ru>
2020-05-02 19:32:55 +0300

    Updated posts controller spec

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-  let(:user) { create(:user) }
+  let(:user_post) { create(:post) }
+  let(:other_user) { create(:user) }
+  let(:user) { user_post.user }
 
   describe 'POST #create' do
     context 'with valid attributes' do
@@ -46,6 +48,81 @@ RSpec.describe PostsController, type: :controller do
                                 post: attributes_for(:post, :invalid) }
 
         expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    context 'with valid attributes' do
+      before do
+        login(user)
+        patch :update, params: { id: user_post, user_id: user, post: { body: 'New body' } }, format: :js
+      end
+
+      it 'assigns the requested post to @post' do
+        expect(assigns(:post)).to eq user_post
+      end
+
+      it 'changes post attributes' do
+        user_post.reload
+        expect(user_post.body).to eq 'New body'
+      end
+
+      it 'renders update view' do
+        expect(response).to render_template :update
+      end
+    end
+
+    context 'with invalid attributes' do
+      before do
+        login(user)
+        patch :update, params: { id: user_post,
+                                 user_id: user,
+                                 post: attributes_for(:post, :invalid) }, format: :js
+      end
+
+      it 'assigns the requested post to @post' do
+        expect(assigns(:post)).to eq user_post
+      end
+
+      it 'does not change post' do
+        user_post.reload
+        expect(user_post.body).to eq 'Post body'
+      end
+
+      it 'renders update view' do
+        expect(response).to render_template :update
+      end
+    end
+
+    context "someone else's post" do
+      before do
+        login(other_user)
+        patch :update, params: { id: user_post, user_id: user, post: { body: 'New body' } }, format: :js
+      end
+
+      it 'does not change post' do
+        user_post.reload
+        expect(user_post.body).to eq 'Post body'
+      end
+
+      it 'redirects to root page' do
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'for unauthenticated user' do
+      before do
+        patch :update, params: { id: user_post, user_id: user, post: { body: 'New body' } }, format: :js
+      end
+
+      it 'does not change post' do
+        user_post.reload
+        expect(user_post.body).to eq 'Post body'
+      end
+
+      it 'status 401: Unauthorized' do
+        expect(response.status).to eq 401
       end
     end
   end
