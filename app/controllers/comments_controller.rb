da52aacb321cae5c51044255b1accdf17c9cfdf0
celtic93 <egorka80@list.ru>
2020-05-05 19:26:20 +0300

    Added update action in controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,15 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_commented
+  before_action :find_commented, only: %i(create)
+  before_action :find_comment, only: %i(update)
 
   def create
     @comment = @commented.comments.create(comment_params.merge(user: current_user))
+  end
+
+  def update
+    redirect_to root_path, alert: 'You can not do this' unless current_user.author?(@comment)
+    @comment.update(comment_params)
   end
 
   private
@@ -15,5 +21,9 @@ class CommentsController < ApplicationController
   def find_commented
     klass = [Post].find {|c| params["#{c.name.underscore}_id"] }
     @commented = klass.find(params["#{klass.name.underscore}_id"])
+  end
+
+  def find_comment
+    @comment = Comment.find(params[:id])
   end
 end
