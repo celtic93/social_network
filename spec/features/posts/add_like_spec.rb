5c2fe747c10f6fdcde0fefd42c4624de4992f585
celtic93 <egorka80@list.ru>
2020-05-11 11:42:12 +0300

    Updated like views

--- spec/features/posts/add_like_spec.rb
+++ spec/features/posts/add_like_spec.rb
@@ -10,7 +10,7 @@ feature 'User can like post' do
     within ".like-post-#{post.id}-link" do
       expect(page).to_not have_content '1 ❤'
       click_on '0 ❤'
-      expect(page).to have_content '1 ❤'
+      expect(page).to have_content '1 💛'
     end
   end
 
