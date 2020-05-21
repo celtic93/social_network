0ab0e3a3c34c97be3d49ba31d25a98311ed1a2fb
celtic93 <egorka80@list.ru>
2020-05-21 06:42:16 +0300

    Swaped specs

--- spec/features/comments/comments/create_spec.rb
+++ spec/features/comments/comments/create_spec.rb
@@ -1,49 +1,55 @@
 require 'rails_helper'
 
-feature 'User can create comment' do
+feature 'User can create comment for comment' do
   given(:post) { create(:post) }
+  given(:comment) { create(:comment, commentable: post) }
   given(:user) { post.user }
 
   describe 'Authenticated user tryes to create comment' do
     scenario 'with valid attributes', js: true do
       sign_in(user)
-      expect(page).to_not have_content 'This is comment'
-      expect(page).to_not have_selector "#comment-form-post-#{post.id}"
 
-      click_on 'Comment'
+      within "#comment-#{comment.id}"
+        expect(page).to_not have_content 'This is comment'
+        expect(page).to_not have_selector "#comment-form-comment-#{comment.id}"
 
-      expect(page).to have_selector "#comment-form-post-#{post.id}"
-
-      within "#comment-form-post-#{post.id}" do
-        fill_in id: "comment-post-#{post.id}", with: 'This is comment'
         click_on 'Comment'
-      end
 
-      expect(page).to have_content 'This is comment'
-      expect(page).to_not have_selector "#comment-form-post-#{post.id}"
+        expect(page).to have_selector "#comment-form-comment-#{comment.id}"
+
+        within "#comment-form-comment-#{comment.id}" do
+          fill_in id: "comment-comment-#{comment.id}", with: 'This is comment'
+          click_on 'Comment'
+        end
+
+        expect(page).to have_content 'This is comment'
+        expect(page).to_not have_selector "#comment-form-comment-#{comment.id}"
+      end
     end
 
     scenario 'with invalid attributes', js: true do
       sign_in(user)
-      
-      expect(page).to_not have_content 'This is comment'
-      expect(page).to_not have_selector "#comment-form-post-#{post.id}"
 
-      click_on 'Comment'
+      within "#comment-#{comment.id}"
+        expect(page).to_not have_content 'This is comment'
+        expect(page).to_not have_selector "#comment-form-comment-#{comment.id}"
 
-      expect(page).to have_selector "#comment-form-post-#{post.id}"
-
-      within "#comment-form-post-#{post.id}" do
-        fill_in id: "comment-post-#{post.id}", with: ''
         click_on 'Comment'
-      end
 
-      expect(page).to_not have_content 'This is comment'
-      expect(page).to have_content "Body can't be blank"
+        expect(page).to have_selector "#comment-form-comment-#{comment.id}"
+
+        within "#comment-form-comment-#{comment.id}" do
+          fill_in id: "comment-comment-#{comment.id}", with: ''
+          click_on 'Comment'
+        end
+
+        expect(page).to_not have_content 'This is comment'
+        expect(page).to have_content "Body can't be blank"
+      end
     end
   end
 
-  scenario 'Unauthenticated user tryes to create post' do
+  scenario 'Unauthenticated user tryes to create comment' do
     visit user_path(user)
     expect(page).to_not have_link 'Comment'
   end
