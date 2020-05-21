c89e38f8eba475f721fac409ef64112f35124e98
celtic93 <egorka80@list.ru>
2020-05-21 03:46:12 +0300

    Added reject request spec

--- spec/features/friendships/reject_spec.rb
+++ spec/features/friendships/reject_spec.rb
@@ -7,17 +7,6 @@ feature 'User can accept request for friendship' do
   given!(:friendship_2) { create(:friendship, :pending, user: friend, friend: user) }
 
   describe 'Authenticated user tryes to accept request', js: true do
-    scenario 'on user profile page', js: true do
-      sign_in(user)
-      visit user_path(friend)
-
-      within '.friend-link' do
-        expect(page).to_not have_content 'Unfriend'
-        click_on 'Accept'
-        expect(page).to have_content 'Unfriend'
-      end
-    end
-
     scenario 'on his friends page', js: true do
       sign_in(user)
       click_on 'My friends'
@@ -30,21 +19,21 @@ feature 'User can accept request for friendship' do
       expect(page).to have_content friend.firstname
       expect(page).to have_content friend.lastname
 
-      click_on 'Accept'
+      click_on 'Reject'
       expect(page).to_not have_content friend.firstname
       expect(page).to_not have_content friend.lastname
 
       click_on 'Friends'
-      expect(page).to have_content friend.firstname
-      expect(page).to have_content friend.lastname
-      expect(page).to have_content 'Unfriend'
+      expect(page).to_not have_content friend.firstname
+      expect(page).to_not have_content friend.lastname
+      expect(page).to_not have_content 'Unfriend'
     end
   end
 
   scenario 'Unauthenticated user tryes to make request' do
     visit user_path(user)
     expect(page).to_not have_selector '.friend-link'
-    expect(page).to_not have_content 'Accept'
+    expect(page).to_not have_content 'Reject'
     expect(page).to_not have_content 'My friends'
   end
 end
