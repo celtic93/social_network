af286ff8783e86b570a946eeda92c2aa8c11a9b6
celtic93 <egorka80@list.ru>
2020-06-12 11:59:01 +0300

    Added communities show and create actions with views

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,15 +1,31 @@
 class CommunitiesController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :find_community, only: %i(show)
+  
   def index
     
   end
 
-  def new
+  def show
     
   end
 
+  def new
+    @community = Community.new
+  end
+
   def create
-    
+    @community = current_user.communities.new(community_params)    
+    @community.save ? redirect_to(@community) : render(:create)
+  end
+
+  private
+
+  def find_community
+    @community = Community.find(params[:id])
+  end
+
+  def community_params
+    params.require(:community).permit(:name, :description)
   end
 end
