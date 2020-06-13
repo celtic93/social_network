48c67f7f12b83c09f3a1f7a325c8423b04acd2c5
celtic93 <egorka80@list.ru>
2020-06-13 17:05:58 +0300

    Updated models specs

--- spec/models/community_spec.rb
+++ spec/models/community_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Community, type: :model do
+  it_behaves_like 'publisher'
+  
   it { should belong_to :user }
 
   it { should validate_presence_of :name }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it_behaves_like 'publisher'
+
   describe 'validations' do
     it { should have_many :posts }
     it { should have_many :comments }
