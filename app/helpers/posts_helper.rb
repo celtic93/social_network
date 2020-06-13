81213455159ec38db9449e9b6084b4f03a06ecc8
celtic93 <egorka80@list.ru>
2020-06-13 16:40:35 +0300

    Updated post view

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,9 @@
 module PostsHelper
+  def publisher_link(post)
+    if post.publisher_type == 'User'
+      link_to "#{post.publisher.firstname} #{post.publisher.lastname}", post.publisher
+    else
+      link_to "#{post.publisher.name}", post.publisher
+    end
+  end
 end
