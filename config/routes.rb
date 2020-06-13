22eab81189c87cb290402f1b23e21bcb40fe9ae0
celtic93 <egorka80@list.ru>
2020-06-13 15:06:23 +0300

    Updated posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  
-  resources :users, except: %i(index new create) do
+
+  concern :publisher do
     resources :posts, shallow: true, only: %i(create edit update destroy) do
       resources :comments, shallow: true, only: %i(create edit update destroy) do
         resources :comments, shallow: true, only: %i(create edit update destroy)
@@ -10,12 +10,14 @@ Rails.application.routes.draw do
       
       resources :likes, shallow: true, only: %i(create destroy)
     end
-    
+  end
+  
+  resources :users, except: %i(index new create), concerns: [:publisher] do
     resources :friendships, only: %i(index create destroy)
   end
 
   resources :friendship_requests, only: %i(create destroy)
-  resources :communities
+  resources :communities, concerns: [:publisher]
 
   root to: 'users#current_user_home'
 end
