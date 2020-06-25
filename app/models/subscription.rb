7e8917bcd2e241df159189078733100290e2bfea
celtic93 <egorka80@list.ru>
2020-06-25 05:41:37 +0300

    Added subscription model spec

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,5 +2,5 @@ class Subscription < ApplicationRecord
   belongs_to :subscriber, class_name: 'User'
   belongs_to :publisher, polymorphic: true
 
-  validates :subscriber, uniqueness: { scope: :publisher }
+  validates :subscriber, uniqueness: { scope: [:publisher_id, :publisher_type] }
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:subscriber).class_name('User') }
+  it { should belong_to :publisher }
+
+  subject { FactoryBot.build(:subscription) }
+  it { should validate_uniqueness_of(:subscriber).scoped_to([:publisher_id, :publisher_type]) }
 end
