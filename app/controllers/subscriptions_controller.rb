b70692e5e90dfaf9defc2d341a9a48814d01b5e2
celtic93 <egorka80@list.ru>
2020-06-26 05:08:35 +0300

    Added destroy action to subscriptions

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,9 +1,18 @@
 class SubscriptionsController < ApplicationController
   before_action :authenticate_user!
   before_action :find_publisher, only: %i(create)
+  before_action :find_subscription, only: %i(destroy)
 
   def create
     @subscription = Subscription.create(subscriber: current_user, publisher: @publisher)
+  end
+
+  def destroy
+    if @subscription
+      @subscription.destroy
+    else
+      redirect_to root_path, alert: 'You can not do this'
+    end
   end
 
   private
@@ -11,5 +20,11 @@ class SubscriptionsController < ApplicationController
   def find_publisher
     klass = [User, Community].find {|c| params["#{c.name.underscore}_id"] }
     @publisher = klass.find(params["#{klass.name.underscore}_id"])
+  end
+
+  def find_subscription
+    klass = [User, Community].find {|c| c.name == params[:publisher_type] }
+    @publisher = klass.find(params[:id])
+    @subscription = current_user.subscriptions.find_by(publisher: @publisher)
   end
 end
